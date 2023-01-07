4fc68673dae0d14cc4772c504db00b9c9ad1d6d8
Osama Ibrahim <osama.ibrahim.sadek@gmail.com>
2023-01-07 14:09:00 +0200

    pull

--- lib/provider/attendance_provider.dart
+++ lib/provider/attendance_provider.dart
@@ -11,12 +11,25 @@ class AttendanceProvider with ChangeNotifier{
 }
 
   Future<void> addAttendance(Attendance attendance) async {
-    final userRepository = AttendanceRepository();
+    final attendanceRepository = AttendanceRepository();
 
-    final int userId = await userRepository.insert(attendance);
+    final int userId = await attendanceRepository.insert(attendance);
     attendance.id = userId;
     _attendanceList.add(attendance);
 
     notifyListeners();
+  }
+
+  late Attendance? _attendanceModel;
+  Attendance? get attendanceModel{
+    return _attendanceModel;
+  }
+  void getAttendanceUserToDay({required int userId,required String date})async{
+    final attendanceRepository = AttendanceRepository();
+    List<Attendance> list =await attendanceRepository.retrieveByUserIdDateTime(userId, date);
+_attendanceModel=list[0];
+print(_attendanceModel);
+notifyListeners();
+
   }
 }
